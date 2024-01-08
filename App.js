173eb95b15fd2c4b3d2b115a785cf23d73998777
AnkhjinDDD <ankhjin8021@gmail.com>
2024-01-08 09:31:16 +0800

    update

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Намайг Анхжин гэдэг. 18 настай. "Codely Academy" -н сурагч. Миний анхны
+        аппын санаа
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
   },
 });
